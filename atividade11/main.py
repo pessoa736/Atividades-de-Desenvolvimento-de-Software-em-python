501030d7fb82a6574d244dbc11d23f02e11d9acd
Davi dos Santos Passos <fenandosouzamelo419@gmail.com>
2025-05-20 22:27:47 -0300

    atividade 11

---
+++ atividade11/main.py
@@ -0,0 +1,20 @@
+# Variáveis de diferentes tipos
+nome = "davi"
+idade = 19
+altura = 1.68
+peso = 75.0
+cidade = "SKIBIDI"
+profissao = "ENGENHEIRO DE SOFTWARE"
+nota_prova = 9.2
+ano_nascimento = 2005
+
+# Frases usando format()
+frase1 = "Meu nome é {} e tenho {} anos.".format(nome, idade)
+frase2 = "Moro em {} e trabalho como {}.".format(cidade, profissao)
+frase3 = "Minha altura é {} metros e peso {} kg.".format(altura, peso)
+frase4 = "Tirei nota {} na prova e nasci em {}.".format(nota_prova, ano_nascimento)
+
+print(frase1)
+print(frase2)
+print(frase3)
+print(frase4)
